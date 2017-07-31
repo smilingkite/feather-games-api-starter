d4754e6918c1cd67951a3fe9b7bce80d03561ee2
smilingkite <mail@katinkahesselink.net>
2017-07-31 15:16:16 +0200

    create game hook setup

--- src/hooks/create-game.js
+++ src/hooks/create-game.js
@@ -1,10 +1,32 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+function shuffle(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter --;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
+
+    const { user } = hook.params;
+
+    hook.data.userId = user._id,
+    hook.data.players = [{
+      userId: user._id,
+      hand: [] // maybe needs to be player_1_hand etc.
+    }];
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
+    var cards = shuffle(Array(40).fill(0).map((e,i)=>i+1));
+
+    hook.data.set = cards;
+
     return Promise.resolve(hook);
   };
 };
