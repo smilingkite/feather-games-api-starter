a89df35d2cefa56664a92b36ebaa0d3b114eb0f0
smilingkite <mail@katinkahesselink.net>
2017-07-31 15:45:00 +0200

    join game hook and game model start

--- src/hooks/join-game.js
+++ src/hooks/join-game.js
@@ -1,35 +1,38 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-function shuffle(array) {
-  let counter = array.length;
-
-  while (counter > 0) {
-    let index = Math.floor(Math.random() * counter);
-    counter --;
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-  return array;
-}
-
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+
+    // see if hook.data has { join: boolean }
+    if (hook.data.join === undefined) return Promise.resolve(hook);
 
     const { user } = hook.params;
 
-    hook.data.userId = user._id,
-    hook.data.players = [{
-      userId: user._id,
-      hand: [] // maybe needs to be player_1_hand etc.
-    }];
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
-    var cards = shuffle(Array(40).fill(0).map((e,i)=>i+1));
+    // see if player already present
+    return hook.app.service('games').get(hook.id)
+      .then((game) => {
+        const { players } = game;
+        const wantsToJoin = hook.data.join;
+        const joined = players.map((p) => (p.userId)).includes(user._id);
 
-    hook.data.set = cards;
+        hook.data = {};
 
-    return Promise.resolve(hook);
+        if (!joined && wantsToJoin) {
+          hook.data = {
+            players: players.concat({ userId: user._id, pairs: [] })
+          };
+        }
+
+        if (joined && !wantsToJoin) {
+          hook.data = {
+            players: players.filter((p) => (p.userId !== user._id))
+          };
+        }
+
+        return Promise.resolve(hook);
+      });
   };
 };
--- src/models/games.model.js
+++ src/models/games.model.js
@@ -1,13 +1,33 @@
 // games-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const games = new mongooseClient.Schema({
-    text: { type: String, required: true },
+  const { Schema } = mongooseClient;
+
+  const cardSchema = new Schema({
+    symbol: { type: Number, required: true },
+    visible: { type: Boolean, default: false },
+    won: { type: Boolean, default: false },
+  });
+
+  const playerSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    pairs: [String],
+    hand: [cardSchema],
+  });
+
+  const games = new Schema({
+    cards: [cardSchema],
+    players: [playerSchema],
+    turn: { type: Number, default: 0 }, // player index
+    started: { type: Boolean, default: false },
+    winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
     createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    updatedAt: { type: Date, default: Date.now },
+    // lastCard: { type: Number },
   });
 
   return mongooseClient.model('games', games);
