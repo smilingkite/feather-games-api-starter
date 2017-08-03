2fb037428ed7be8d8a0bd57dea937fa3c555b87a
smilingkite <mail@katinkahesselink.net>
2017-08-03 17:14:40 +0200

    compare cards

--- src/hooks/compare-cards.js
+++ src/hooks/compare-cards.js
@@ -4,20 +4,30 @@
 // on selectedCards > compareCards > select winner of round.
 
 function compareCards(Card1, Card2) {
-  if (Card1 > Card2) return Card1 ;
-  return Card2;
-  // set 'won' attribute  on card to true;
+  if (Card1.symbol > Card2.symbol) return 'Card1' ;
+  return 'Card2';
 }
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
-    console.log("compareCards hook called")
+    console.log("compareCards hook called") // eslint-disable-line
     if (hook.data.player.hand.selectedCard !== undefined) return Promise.resolve(hook);
 
     return hook.app.service('games').get(hook.id)
       .then((game) => {
-        const { players, hand, cards } = game;
+        const { hand1, hand2 , score1, score2 } = game;
+        var Card1 = hand1.filter(function(hand){
+          return hand.selected;
+        });
+        var Card2 = hand2.filter(function(hand){
+          return hand.selected;
+        });
 
+        if (compareCards(Card1, Card2) === 'Card1'){
+          score1+1;
+        } else {
+          score2+1;
+        }
       });
 
     // Hooks can either return nothing or a promise
